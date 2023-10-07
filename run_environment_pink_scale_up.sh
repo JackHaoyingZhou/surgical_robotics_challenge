0199223bf6dbe110d9b2c48a63bccf35530f95ff
JackHaoyingZhou <zhouhaoying96@outlook.com>
2023-10-07 05:45:04 -0400

    add new needle and scale old needle

---
+++ run_environment_pink_scale_up.sh
@@ -0,0 +1,2 @@
+# source /home/practicepoint/daVinci_simulation/src/ambf/build/devel/setup.bash
+~/daVinci_simulation/src/ambf/bin/lin-x86_64/ambf_simulator --launch_file ~/surgical_jmrr/surgical_robotics_challenge/launch.yaml -l 0,1,3,4,14,22 -p120 -t1 --override_max_comm_freq 120 --override_min_comm_freq 100
